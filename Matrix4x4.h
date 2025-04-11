6d543c0996ef0a6325fff1d6dfa0eb1a2dccb36d
玲亜 星宮 <taiga040505@gmail.com>
2025-04-11 12:40:52 +0900

    前方宣言の追加

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -4,6 +4,9 @@ struct Matrix4x4 {
 	float m[4][4];
 };
 
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
 //行列の加法
 Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
 //行列の減法
@@ -16,3 +19,6 @@ Matrix4x4 Inverse(const Matrix4x4& m);
 Matrix4x4 Transpose(const Matrix4x4& m);
 //単位行列の生成
 Matrix4x4 MakeIdentity4x4();
+
+//描画関数
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& m);
